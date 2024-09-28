ae936c38a3c770c0d1f1fb984aee3928d316036d
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-28 18:27:30 -0300

    User related Records

--- src/main/java/santi/parra/runnerz/Application.java
+++ src/main/java/santi/parra/runnerz/Application.java
@@ -1,41 +1,25 @@
 package santi.parra.runnerz;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import santi.parra.runnerz.user.UserHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import santi.parra.runnerz.run.Location;
-import santi.parra.runnerz.run.Run;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
-
 	}
 
 	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			Run run = new Run(
-					1,
-					"run",
-					LocalDateTime.now(),
-					LocalDateTime.now().plus(1, ChronoUnit.HOURS),
-					3,
-					Location.OUTDOOR
-			);
-			log.info("Run : " + run);
-		};
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
 	}
-
 }
---
+++ src/main/java/santi/parra/runnerz/user/Company.java
@@ -0,0 +1,4 @@
+package santi.parra.runnerz.user;
+
+public record Company(String name, String catchPhrase, String bs) {
+}
